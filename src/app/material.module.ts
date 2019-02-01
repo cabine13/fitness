6d618a8c3e98e680a49cd0a538d11ce40b4e6e00
cabine13 <frbos@free.fr>
2019-02-01 12:42:39 +0100

    welcome component, NewTraining et current training
    ajout du Welcome screen responsive
    ajout d'un tabs component pour Training
    Travail sur NewTraining et current training:
        card
        dropDown menu
        dialog box

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -10,6 +10,11 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatListModule} from '@angular/material/list';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatCardModule} from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @NgModule({
     imports: [
@@ -26,6 +31,11 @@ import {MatListModule} from '@angular/material/list';
         MatSidenavModule,
         MatToolbarModule,
         MatListModule,
+        MatTabsModule,
+        MatCardModule,
+        MatSelectModule,
+        MatProgressSpinnerModule,
+        MatDialogModule,
     ]
 })
 export class MaterialModule { }
