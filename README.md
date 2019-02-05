f7eeb0b43c46d4c8bec21684f21afa9c0d77f7c4
cabine13 <frbos@free.fr>
2019-02-05 16:55:33 +0100

    Affichage des menus de la sidebar suivant si user connecté ou pas.
    Affichage des menus de la sidebar suivant si user connecté ou pas.

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # Fitness
 
-Installation de rxjs-compat
-Mise en place du service Auth (sans BDD)
-Ajout des modeles User et Auth-Data
-Affichage des menus de la navBar suivant si user connecté ou pas.
--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,18 +1,30 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
+import { AuthService } from './../../services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.scss']
 })
-export class SidenavListComponent implements OnInit {
+export class SidenavListComponent implements OnInit, OnDestroy {
   @Output() closeSidenav = new EventEmitter<void>();
-  constructor() { }
-
+  isAuth = false;
+  authSubscription: Subscription
+  
+  constructor(private authService: AuthService) { }
+  
   ngOnInit() {
+    this.authSubscription = this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    });
+  }
+  
+  ngOnDestroy(): void {
+    this.authSubscription.unsubscribe();
   }
 
-  onClose(){
+  onClose() {
     this.closeSidenav.emit();
   }
 
